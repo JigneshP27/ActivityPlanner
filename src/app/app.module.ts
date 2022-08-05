2c3a0bfd9c4ee5343347ab448585172f63aff5b1
rhydham1411 <rhydham.bhalodia@outlook.com>
2022-08-05 18:39:12 +0530

    right side coponent is add and background color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,19 @@ import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { RightsideComponent } from './rightside/rightside.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LeftsideComponent } from './leftside/leftside.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    RightsideComponent
+    RightsideComponent,
+    LeftsideComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
